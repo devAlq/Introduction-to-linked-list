808e7ee84243d2552cfd1de35ce3bd3fcc8cfe33
devAlq <khalidalqdev@gmail.com>
2024-02-10 17:24:06 +0300

    TaskDone

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
-
+ 
 public class Main {
      public static void main(String[] args) {
-
-     /* your code here */
+        Node firstNode = new Node(43);
+        Node secondNode = new Node(78);
+        Node thirdNode = new Node(97);
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        
+        
+          
+           
 
     }
 }
